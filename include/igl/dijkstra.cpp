e3b2446c1dbcea9715bb9325f4629140d3bbdb68
wuhan <340473925@qq.com>
2022-09-01 18:01:17 +0800

    ..

--- include/igl/dijkstra.cpp
+++ include/igl/dijkstra.cpp
@@ -1,128 +1,153 @@
 #include "dijkstra.h"
 
+
+// dijkstra()重载1
 template <typename IndexType, typename DerivedD, typename DerivedP>
 IGL_INLINE int igl::dijkstra(
-  const IndexType &source,
+  const IndexType &startIdx,
   const std::set<IndexType> &targets,
   const std::vector<std::vector<IndexType> >& adjList,
   const std::vector<double>& weights,
-  Eigen::PlainObjectBase<DerivedD> &min_distance,
+  Eigen::PlainObjectBase<DerivedD> &minDis,
   Eigen::PlainObjectBase<DerivedP> &previous)
 {
-  int numV = adjList.size();
-  min_distance.setConstant(numV, 1, std::numeric_limits<typename DerivedD::Scalar>::max());
-  min_distance[source] = 0;
-  previous.setConstant(numV, 1, -1);
+  int versCount = adjList.size();
+  minDis.setConstant(versCount, 1, std::numeric_limits<typename DerivedD::Scalar>::max());
+  minDis[startIdx] = 0;
+  previous.setConstant(versCount, 1, -1);
   std::set<std::pair<typename DerivedD::Scalar, IndexType> > vertex_queue;
-  vertex_queue.insert(std::make_pair(min_distance[source], source));
+  vertex_queue.insert(std::make_pair(minDis[startIdx], startIdx));
 
   while (!vertex_queue.empty())
   {
     typename DerivedD::Scalar dist = vertex_queue.begin()->first;
-    IndexType u = vertex_queue.begin()->second;
+    IndexType index0 = vertex_queue.begin()->second;
     vertex_queue.erase(vertex_queue.begin());
 
-    if (targets.find(u)!= targets.end())
-      return u;
+    if (targets.find(index0)!= targets.end())
+      return index0;
 
-    // Visit each edge exiting u
-    const std::vector<int> &neighbors = adjList[u];
-    for (std::vector<int>::const_iterator neighbor_iter = neighbors.begin();
-         neighbor_iter != neighbors.end();
-         neighbor_iter++)
+    // Visit each edge exiting index0
+    const std::vector<int> &adjVersIdx0 = adjList[index0];
+    for (std::vector<int>::const_iterator adjIter = adjVersIdx0.begin();
+         adjIter != adjVersIdx0.end();
+         adjIter++)
     {
-      IndexType v = *neighbor_iter;
-      typename DerivedD::Scalar distance_through_u = dist + weights[u];
-      if (distance_through_u < min_distance[v])
+      IndexType adjIdx0 = *adjIter;
+      typename DerivedD::Scalar pathLen = dist + weights[index0];
+      if (pathLen < minDis[adjIdx0])
       {
-        vertex_queue.erase(std::make_pair(min_distance[v], v));
+        vertex_queue.erase(std::make_pair(minDis[adjIdx0], adjIdx0));
 
-        min_distance[v] = distance_through_u;
-        previous[v] = u;
-        vertex_queue.insert(std::make_pair(min_distance[v], v));
-
+        minDis[adjIdx0] = pathLen;
+        previous[adjIdx0] = index0;
+        vertex_queue.insert(std::make_pair(minDis[adjIdx0], adjIdx0));
       }
-
     }
   }
   //we should never get here
   return -1;
 }
 
+
+// dijkstra()重载1.1
 template <typename IndexType, typename DerivedD, typename DerivedP>
 IGL_INLINE int igl::dijkstra(
-  const IndexType &source,
+  const IndexType &startIdx,
   const std::set<IndexType> &targets,
   const std::vector<std::vector<IndexType> >& adjList,
-  Eigen::PlainObjectBase<DerivedD> &min_distance,
+  Eigen::PlainObjectBase<DerivedD> &minDis,
   Eigen::PlainObjectBase<DerivedP> &previous)
 {
   std::vector<double> weights(adjList.size(), 1.0);
-  return dijkstra(source, targets, adjList, weights, min_distance, previous);
+  return dijkstra(startIdx, targets, adjList, weights, minDis, previous);
 }
 
+
+// dijkstra()重载2——路径向量改写为std::vector表示；
 template <typename IndexType, typename DerivedP>
 IGL_INLINE void igl::dijkstra(
   const IndexType &vertex,
   const Eigen::MatrixBase<DerivedP> &previous,
   std::vector<IndexType> &path)
 {
-  IndexType source = vertex;
+  IndexType startIdx = vertex;
   path.clear();
-  for ( ; source != -1; source = previous[source])
-    path.push_back(source);
+  for ( ; startIdx != -1; startIdx = previous[startIdx])
+    path.push_back(startIdx);
 }
 
 
+// dijkstra()重载3
 template <typename IndexType, typename DerivedV,
 typename DerivedD, typename DerivedP>
 IGL_INLINE int igl::dijkstra(
-  const Eigen::MatrixBase<DerivedV> &V,
+  const Eigen::MatrixBase<DerivedV> &vers,
   const std::vector<std::vector<IndexType> >& adjList,
-  const IndexType &source,
+  const IndexType &startIdx,
   const std::set<IndexType> &targets,
-  Eigen::PlainObjectBase<DerivedD> &min_distance,
+  Eigen::PlainObjectBase<DerivedD> &minDis,
   Eigen::PlainObjectBase<DerivedP> &previous)
 {
-  int numV = adjList.size();
+    /*
+    int igl::dijkstra(                                                                                                 返回路径最短的终点索引；不输入终点时，返回-1
+                const Eigen::MatrixBase<DerivedV> &vers,                                        图的点集
+                const std::vector<std::vector<IndexType> >& adjList,                        邻接表
+                const IndexType &startIdx,                                                                   起点索引
+                const std::set<IndexType> &targets,                                                    终点索引，可传入多个；
+                Eigen::PlainObjectBase<DerivedD> &minDis,        若targets为空，则为每个顶点到起点最短路径的长度向量；若不为空，则最小生成树未生长到的顶点长度为inf   
+                Eigen::PlainObjectBase<DerivedP> &previous      若targets为空，则previous为最小生成树向量；若不为空，则为生长到最近终点时的最小生成树；
+                  )
+        
+    */
 
-  min_distance.setConstant(numV, 1, std::numeric_limits<typename DerivedD::Scalar>::infinity());
-  min_distance[source] = 0;
-  previous.setConstant(numV, 1, -1);
-  std::set<std::pair<typename DerivedD::Scalar, IndexType> > vertex_queue;
-  vertex_queue.insert(std::make_pair(min_distance[source], source));
+  int versCount = adjList.size();
 
+  minDis.setConstant(versCount, 1, std::numeric_limits<typename DerivedD::Scalar>::infinity());    // 初始最小距离设为inf
+  minDis[startIdx] = 0;
+  previous.setConstant(versCount, 1, -1);
+  std::set<std::pair<typename DerivedD::Scalar, IndexType> > vertex_queue;      // working queue; (minDis, index)
+  vertex_queue.insert(std::make_pair(minDis[startIdx], startIdx));
+
+  // working queue内的循环：
   while (!vertex_queue.empty())
   {
-    typename DerivedD::Scalar dist = vertex_queue.begin()->first;
-    IndexType u = vertex_queue.begin()->second;
+    typename DerivedD::Scalar dist = vertex_queue.begin()->first;           // 队首元素顶点index0到startIdx最小距离；
+    IndexType index0 = vertex_queue.begin()->second;
+
+    // 1. popHead();
     vertex_queue.erase(vertex_queue.begin());
 
-    if (targets.find(u)!= targets.end())
-      return u;
+    if (targets.find(index0)!= targets.end())
+      return index0;
 
-    // Visit each edge exiting u
-    const std::vector<int> &neighbors = adjList[u];
-    for (std::vector<int>::const_iterator neighbor_iter = neighbors.begin();
-         neighbor_iter != neighbors.end();
-         neighbor_iter++)
+    // 2. 遍历index0的1领域内所有顶点；即遍历顶点index0关联的所有边；
+    const std::vector<int> &adjVersIdx0 = adjList[index0];
+    for (std::vector<int>::const_iterator adjIter = adjVersIdx0.begin(); adjIter != adjVersIdx0.end(); adjIter++)   
     {
-      IndexType v = *neighbor_iter;
-      typename DerivedD::Scalar distance_through_u = dist + (V.row(u) - V.row(v)).norm();
-      if (distance_through_u < min_distance[v]) {
-        vertex_queue.erase(std::make_pair(min_distance[v], v));
+      IndexType adjIdx0 = *adjIter;
 
-        min_distance[v] = distance_through_u;
-        previous[v] = u;
-        vertex_queue.insert(std::make_pair(min_distance[v], v));
+      // 2.1 计算经过当前边(index0, adjIdx0)之后累计的路径长度；
+      typename DerivedD::Scalar pathLen = dist + (vers.row(index0) - vers.row(adjIdx0)).norm();
 
+      // 2.2 若使用当前边，总路径长度比之前的小，则使用当前边；
+      if (pathLen < minDis[adjIdx0])
+      {
+        vertex_queue.erase(std::make_pair(minDis[adjIdx0], adjIdx0));
+
+        minDis[adjIdx0] = pathLen;
+        previous[adjIdx0] = index0;         
+        vertex_queue.insert(std::make_pair(minDis[adjIdx0], adjIdx0));        // queue pushHead()
       }
-
     }
   }
+
   return -1;
 }
 
+
+
+// 模板特化：
 #ifdef IGL_STATIC_LIBRARY
 // Explicit template instantiation
 template int igl::dijkstra<int, Eigen::Matrix<double, -1, 1, 0, -1, 1>, Eigen::Matrix<int, -1, 1, 0, -1, 1> >(int const&, std::set<int, std::less<int>, std::allocator<int> > const&, std::vector<std::vector<int, std::allocator<int> >, std::allocator<std::vector<int, std::allocator<int> > > > const&, Eigen::PlainObjectBase<Eigen::Matrix<double, -1, 1, 0, -1, 1> >&, Eigen::PlainObjectBase<Eigen::Matrix<int, -1, 1, 0, -1, 1> >&);
--- include/igl/dijkstra.h
+++ include/igl/dijkstra.h
@@ -8,25 +8,26 @@
 
 namespace igl {
 
-  // Dijkstra's algorithm for vertex-weighted shortest paths, with multiple targets.
-  // Adapted from http://rosettacode.org/wiki/Dijkstra%27s_algorithm .
-  //
-  // Inputs:
-  //   source           index of source vertex
-  //   targets          target vector set
-  //   VV               #V list of lists of incident vertices (adjacency list), e.g.
-  //                    as returned by igl::adjacency_list
-  //   weights          #V list of scalar vertex weights
-  //
-  // Output:
-  //   min_distance     #V by 1 list of the minimum distances from source to all vertices
-  //   previous         #V by 1 list of the previous visited vertices (for each vertex) - used for backtracking
-  //
+    /*
+       Dijkstra's algorithm for vertex-weighted shortest paths, with multiple targets.
+       Adapted from http://rosettacode.org/wiki/Dijkstra%27s_algorithm .
+  
+       Inputs:
+         source           index of source vertex
+         targets          target vector set
+         adjList               #vers list of lists of incident vertices (adjacency list), e.g.  as returned by igl::adjacency_list
+         weights          #vers list of scalar vertex weights
+  
+       Output:
+         min_distance     #vers by 1 list of the minimum distances from source to all vertices
+         previous           list of the previous visited vertices (for each vertex) - used for backtracking
+  
+    */
   template <typename IndexType, typename DerivedD, typename DerivedP>
   IGL_INLINE int dijkstra(
     const IndexType &source,
     const std::set<IndexType> &targets,
-    const std::vector<std::vector<IndexType> >& VV,
+    const std::vector<std::vector<IndexType> >& adjList,
     const std::vector<double>& weights,
     Eigen::PlainObjectBase<DerivedD> &min_distance,
     Eigen::PlainObjectBase<DerivedP> &previous);
@@ -38,18 +39,18 @@ namespace igl {
   // Inputs:
   //   source           index of source vertex
   //   targets          target vector set
-  //   VV               #V list of lists of incident vertices (adjacency list), e.g.
+  //   adjList               #vers list of lists of incident vertices (adjacency list), e.g.
   //                    as returned by igl::adjacency_list
   //
   // Output:
-  //   min_distance     #V by 1 list of the minimum distances from source to all vertices
-  //   previous         #V by 1 list of the previous visited vertices (for each vertex) - used for backtracking
+  //   min_distance     #vers by 1 list of the minimum distances from source to all vertices
+  //   previous         #vers by 1 list of the previous visited vertices (for each vertex) - used for backtracking
   //
   template <typename IndexType, typename DerivedD, typename DerivedP>
   IGL_INLINE int dijkstra(
     const IndexType &source,
     const std::set<IndexType> &targets,
-    const std::vector<std::vector<IndexType> >& VV,
+    const std::vector<std::vector<IndexType> >& adjList,
     Eigen::PlainObjectBase<DerivedD> &min_distance,
     Eigen::PlainObjectBase<DerivedP> &previous);
 
@@ -58,7 +59,7 @@ namespace igl {
   //
   // Inputs:
   //   vertex           vertex to which we want the shortest path (from same source as above)
-  //   previous         #V by 1 list of the previous visited vertices (for each vertex) - result of Dijkstra's algorithm
+  //   previous         #vers by 1 list of the previous visited vertices (for each vertex) - result of Dijkstra's algorithm
   //
   // Output:
   //   path             #P by 1 list of vertex indices in the shortest path from vertex to source
@@ -70,24 +71,26 @@ namespace igl {
     std::vector<IndexType> &path);
 
 
-  // Dijkstra's algorithm for shortest paths on a mesh, with multiple targets, using edge length
-  //
-  // Inputs:
-  //   V                #V by 3 list of vertex positions
-  //   VV               #V list of lists of incident vertices (adjacency list), e.g.
-  //                    as returned by igl::adjacency_list, will be generated if empty.
-  //   source           index of source vertex
-  //   targets          target vector set
-  //
-  // Output:
-  //   min_distance     #V by 1 list of the minimum distances from source to all vertices
-  //   previous         #V by 1 list of the previous visited vertices (for each vertex) - used for backtracking
-  //
+  
+  /*
+       Dijkstra's algorithm for shortest paths on a mesh, with multiple targets, using edge length
+  
+       Inputs:
+         vers                #vers by 3 list of vertex positions
+         adjList               #vers list of lists of incident vertices (adjacency list), e.g.
+                          as returned by igl::adjacency_list, will be generated if empty.
+         source           index of source vertex
+         targets          target vector set
+  
+       Output:
+         min_distance     #vers by 1 list of the minimum distances from source to all vertices
+         previous         #vers by 1 list of the previous visited vertices (for each vertex) - used for backtracking
+  */
   template <typename IndexType, typename DerivedV,
   typename DerivedD, typename DerivedP>
   IGL_INLINE int dijkstra(
-    const Eigen::MatrixBase<DerivedV> &V,
-    const std::vector<std::vector<IndexType> >& VV,
+    const Eigen::MatrixBase<DerivedV> &vers,
+    const std::vector<std::vector<IndexType> >& adjList,
     const IndexType &source,
     const std::set<IndexType> &targets,
     Eigen::PlainObjectBase<DerivedD> &min_distance,
