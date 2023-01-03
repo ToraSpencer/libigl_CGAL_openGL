34ee1cac36d662c346ce3f6986f43144d7219ed4
wuhan <340473925@qq.com>
2023-01-03 18:35:05 +0800

    ..

--- include/igl/connected_components.h
+++ include/igl/connected_components.h
@@ -1,31 +1,35 @@
-// This file is part of libigl, a simple c++ geometry processing library.
-//
-// Copyright (C) 2020 Alec Jacobson <alecjacobson@gmail.com>
-//
-// This Source Code Form is subject to the terms of the Mozilla Public License
-// v. 2.0. If a copy of the MPL was not distributed with this file, You can
-// obtain one at http://mozilla.org/MPL/2.0/.
 #ifndef IGL_CONNECTED_COMPONENTS_H
 #define IGL_CONNECTED_COMPONENTS_H
 #include "igl_inline.h"
 #include <Eigen/Core>
 #include <Eigen/Sparse>
+
+
 namespace igl
 {
-  // Determine the connected components of a graph described by the input
-  // adjacency matrix (similar to MATLAB's graphconncomp).
-  //
-  // Inputs:
-  //    A  #A by #A adjacency matrix (treated as describing an undirected graph)
-  // Outputs:
-  //    C  #A list of component indices into [0,#K-1]
-  //    K  #K list of sizes of each component
-  // Returns number of connected components
+    /*
+       Determine the connected components of a graph described by the input adjacency matrix (similar to MATLAB's graphconncomp).
+  
+       Inputs:
+          adjSM  #adjSM by #adjSM adjacency matrix (treated as describing an undirected graph)
+                                     网格的无向边邻接矩阵；
+
+       Outputs:
+          connectedLabels  #adjSM list of component indices into [0,#connectedCount-1]
+                                        同一连通区域内的顶点标签，依次为0, 1, 2,...
+                                        索引为i的顶点的标签为connectedLabels(i);
+
+          connectedCount  #connectedCount list of sizes of each component
+                                        每个标签对应的单连通区域内包含的顶点数
+                                        标签为i的单连通区域包含的顶点数为connectedCount(i)
+       
+       Returns number of connected components
+    */
   template < typename Atype, typename DerivedC, typename DerivedK>
   IGL_INLINE int connected_components(
-    const Eigen::SparseMatrix<Atype> & A,
-    Eigen::PlainObjectBase<DerivedC> & C,
-    Eigen::PlainObjectBase<DerivedK> & K);
+    const Eigen::SparseMatrix<Atype> & adjSM,
+    Eigen::PlainObjectBase<DerivedC> & connectedLabels,
+    Eigen::PlainObjectBase<DerivedK> & connectedCount);
 }
 
 #ifndef IGL_STATIC_LIBRARY
